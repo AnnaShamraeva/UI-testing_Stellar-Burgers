fa6dac941a794721ac121311fbb137f8757107e4
Anna Shamraeva <anna_plenkina@mail.ru>
2025-11-27 15:53:26 +0300

    Tests in development v5

--- conftest.py
+++ conftest.py
@@ -9,11 +9,15 @@ from locators import Locators
 
 @pytest.fixture(scope='function')
 def driver():
-    options = Options()
-    options.add_argument('--window-size=1920,1080') # добавили ещё настройку —-window-size запускает браузер с заданным разрешением экрана.
+    # options = Options()
+    # options.add_argument('--window-size=1920,1080') # добавили ещё настройку —-window-size запускает браузер с заданным разрешением экрана.
     # Полезно, когда нужно протестировать интерфейс в системе с определённым разрешением.
-    service = Service("C:\WebDriver\bin\chromedriver.exe")
-    browser = webdriver.Chrome(options=options, service=service)
-    browser.get(Url.main_site)
-    yield browser
-    browser.quit()
+    # service = Service(r"C:\WebDriver\bin\chromedriver.exe")
+    # browser = webdriver.Chrome(options=options, service=service)
+    # browser.get(Url.main_site)
+    driver = webdriver.Chrome()
+    driver.get(Url.main_site)
+    #yield browser
+    #browser.quit()
+    yield driver
+    driver.quit()
